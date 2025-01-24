c5c65708012164cd1db251847403e1e908b25eaa
Andreas Klebe <a.klebe.work@gmail.com>
2025-01-24 10:42:02 +0100

    - fixed image uploads  - for nom media item always a new media item will be created instead to change the image - Import status results - adjusted Form descriptions - fixed product form is_test default

--- app/Events/ImportContent.php
+++ app/Events/ImportContent.php
@@ -5,6 +5,7 @@ namespace Modules\DeployEnv\app\Events;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Modules\SystemBase\app\Models\JsonViewResponse;
 
 class ImportContent
 {
@@ -26,12 +27,22 @@ class ImportContent
     public ?int $forceUserId = null;
 
     /**
+     * @var JsonViewResponse|null
+     */
+    public ?JsonViewResponse $results = null;
+
+    /**
      * Create a new event instance.
      */
-    public function __construct(string $type, array $sourcePathInfo, ?int $forceUserId = null)
+    public function __construct(string $type, array $sourcePathInfo, ?int $forceUserId = null, $resultObject = null)
     {
         $this->type = $type;
         $this->sourcePathInfo = $sourcePathInfo;
         $this->forceUserId = $forceUserId;
+
+        if ($resultObject === null) {
+            $resultObject = new JsonViewResponse();
+        }
+        $this->results = $resultObject;
     }
 }
